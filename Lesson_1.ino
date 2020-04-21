ea828d2147bd08a868a0c841447493c8c4783c27
aeaeJacky <ckleungjacky@gmail.com>
2020-04-22 02:01:17 +0800

    Add files via upload

---
+++ Lesson_1.ino
@@ -0,0 +1,14 @@
+// Lesson 1 solution
+
+int ledPin = 13;
+
+void setup() {
+   pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+   digitalWrite(ledPin, HIGH);
+   delay(1000);
+   digitalWrite(ledPin, LOW);
+   delay(1000);  
+}
